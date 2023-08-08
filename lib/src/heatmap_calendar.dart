6e9c2d3a900944eb04bde36cacfa42d9b0749b59
yoondabin <yundabin0608@gmail.com>
2023-08-07 23:15:48 -0400

    FIX : header 수정

--- lib/src/heatmap_calendar.dart
+++ lib/src/heatmap_calendar.dart
@@ -154,9 +154,9 @@ class _HeatMapCalendar extends State<HeatMapCalendar> {
 
         // Text which shows the current year and month
         Text(
-          DateUtil.MONTH_LABEL[_currentDate?.month ?? 0] +
+          (_currentDate?.year).toString() +
               ' ' +
-              (_currentDate?.year).toString(),
+              DateUtil.MONTH_LABEL[_currentDate?.month ?? 0],
           style: TextStyle(
             fontSize: widget.monthFontSize ?? 12,
           ),
